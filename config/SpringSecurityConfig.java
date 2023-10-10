3f9bdbe51b23779b20505f8189a5f51468e9c249
garima <garimasrivastava0104@gmail.com>
2023-10-10 14:38:33 +0530

    Added Spring Security using Method Level Security

--- config/SpringSecurityConfig.java
+++ config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,7 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
+@EnableMethodSecurity
 @Configuration
 //this class becomes spring java based configuration within this class we can define the spring bean
 public class SpringSecurityConfig {
@@ -28,14 +29,14 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http.csrf((csrf)->csrf.disable()).authorizeHttpRequests((authorize) -> {
-            authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
-            authorize.requestMatchers(HttpMethod.PUT,"/api/**").hasRole("ADMIN");
-            authorize.requestMatchers(HttpMethod.DELETE,"/api/**").hasRole("ADMIN");
-//            To provide more than one role to the users use the method named as has any role method
-            authorize.requestMatchers(HttpMethod.GET,"/api**").hasAnyRole("ADMIN","niraj");
-            authorize.requestMatchers(HttpMethod.PATCH,"/api/**").hasAnyRole("ADMIN","niraj");
-//            Providing public access to get api for that we have permitAll method for these we don't have to provide the user credentials
-            authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll();
+//            authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+//            authorize.requestMatchers(HttpMethod.PUT,"/api/**").hasRole("ADMIN");
+//            authorize.requestMatchers(HttpMethod.DELETE,"/api/**").hasRole("ADMIN");
+////            To provide more than one role to the users use the method named as has any role method
+//            authorize.requestMatchers(HttpMethod.GET,"/api**").hasAnyRole("ADMIN","niraj");
+//            authorize.requestMatchers(HttpMethod.PATCH,"/api/**").hasAnyRole("ADMIN","niraj");
+////            Providing public access to get api for that we have permitAll method for these we don't have to provide the user credentials
+//            authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll();
             authorize.anyRequest().authenticated();
         }).httpBasic(Customizer.withDefaults());
                 return http.build();
--- controller/TodoController.java
+++ controller/TodoController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,18 +18,21 @@ public class TodoController {
     @Autowired
     private TodoService todoService;
 //    Build add todo Rest API
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
        TodoDto savedtodoDto=todoService.addTodo(todoDto);
        return new ResponseEntity<>(savedtodoDto, HttpStatus.CREATED);
     }
     //Build get todo REST API
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("/{id}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable Long id){
         TodoDto reqTodo=todoService.getTodo(id);
         return new ResponseEntity<>(reqTodo,HttpStatus.OK);
     }
     //Build get All todo REST API
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping
     public ResponseEntity<List<TodoDto>> getAllTodo(){
         List<TodoDto> reqTodo=todoService.getAllTodo();
@@ -37,24 +41,28 @@ public class TodoController {
         return ResponseEntity.ok(reqTodo);
     }
     //Build update todo REST API
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<TodoDto> updateTodo(@RequestBody TodoDto todoDto,@PathVariable Long id){
         TodoDto todo=todoService.updateTodo(todoDto,id);
         return ResponseEntity.ok(todo);
     }
     //Build delete todo REST API
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<TodoDto> deleteTodo(@PathVariable Long id){
       TodoDto deletedTodo=todoService.deleteTodo(id);
         return ResponseEntity.ok(deletedTodo);
     }
     //Build Complete Todo REST API
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("/comp/{id}")
     public ResponseEntity<TodoDto> completeTodo(@PathVariable Long id){
         TodoDto compTodo=todoService.completeTodo(id);
         return ResponseEntity.ok(compTodo);
     }
     //Build inComplete Todo REST API
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
 //    all the complete method and incomplete method has the patch mapping
 // IMPORTANT :-->   PUT is a technique of altering resources when the client transmits data that revamps the whole resource.
 // PATCH is a technique for transforming the resources when the client transmits partial data that will be updated without changing the whole data.
